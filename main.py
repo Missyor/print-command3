786457cf13a52f3d217dca23361c701538ba16cd
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 14:44:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+##To display a message we use print ("")
+print("Hello")
+print("my name is Aoife")
+
+##input lets the user put in information
+##we created a variable. I called it name
+##Variables store the information that the user enters
+name = input("What is your name?: ")
+
+##display a message saying hello and the information the user input
+print("hello", name)
